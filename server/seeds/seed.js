bb0927875ef4f7c4dcb9249db5163a62ac8697ce
Dana Alami <dana.alami18@gmail.com>
2021-07-28 19:23:03 +0300

    created seed fils with userdata and weddingdata files

---
+++ server/seeds/seed.js
@@ -0,0 +1,21 @@
+const db = require('../config/connection');
+const { User, wedding } = require('../models');
+
+const userData = require('./userData.json');
+const weddingData = require('./weddingData.json');
+
+db.once('open', async () => {
+    await wedding.deleteMany({});
+
+    const couples = await wedding.inerstMany(weddingData);
+    console.log('couples seeded!')
+    process.exit(0);
+})
+
+db.once('open', async () => {
+    await User.deleteMany({});
+
+    const users = await User.insertMany(userData);
+    console.log('users seeded!')
+    process.exit(0);
+})

eded4ef3f7540a5befd601781a52fa1fae7c7387
Harith Albadry <harith.albadry@gmail.com>
2021-07-28 15:13:58 +0100

    created horizontal scrolling page for landing plus uploaded own font

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Guests from './components/Guests'
 import Home from './components/Home'
+import Landing from './components/landing/landing.page';
 
 
 
@@ -12,6 +13,7 @@ import Home from './components/Home'
         <div className="App">
           <Route exact path='/' component={Guests} /> 
           <Route exact path='/Home' component={Home} />
+          <Route exact path="/landing" component={Landing} />
         </div>
         </Router>
       );

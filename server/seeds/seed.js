58c6d14af8c340e40d54796adce378b64282451c
notfound <notfound@pop-os.localdomain>
2021-08-02 21:13:11 +0100

    FIX : updates the seed and seed script

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,4 +1,4 @@
-const db = require('../../config/connection');
+const db = require('../config/connection');
 const { User, wedding, Guests, Wedding } = require('../models');
 
 const userData = require('./userData.json');

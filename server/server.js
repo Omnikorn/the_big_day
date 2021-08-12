db2f3505b22703229a18c0057ccb67b73e4a1cfd
lloret82 <adbmanchester@gmail.com>
2021-08-12 17:50:32 +0100

    test

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const cors = require ("cors")
-
+const cors = require("cors")
+const mongoose = require("mongoose")
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+const bodyParser = require("body-parser");
+ 
+require("dotenv").config()
+
+    mongoose
+     .connect(
+         process.env.MONGODB_CONNECTION_STRING,
+             {
+               useNewUrlParser: true,
+               useUnifiedTopology: true,
+             }
+     )
+     .then(() => console.log("MongoDB has been connected"))
+     .catch((err) => console.log(err));
 
 
-const PORT = process.env.PORT || 3001;
+
+const PORT = process.env.PORT || 5000
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -15,8 +30,17 @@ const server = new ApolloServer({
   // context: authMiddleware,
 });
 
-server.applyMiddleware({ app })
 
+server.applyMiddleware({ app })
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -27,3 +51,13 @@ db.once('open', () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+
+mongoose
+  .connect(
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
